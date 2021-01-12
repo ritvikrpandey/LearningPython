c5748a4785a4aacafa7d4ac051874c967039f1a3
[Ritvik] <[ritvikrpandey@gmail.com]>
2021-01-12 00:43:57 +0000

    #Lets do a name exchange and try to talk smart print('Welcome to the basic naming conversation!') print('My name is Etc!') print('What is your name?') yourName=input() print('It is nice to meet you ' + yourName + '.') print('It appears so that your name is ' + str(len(yourName)) + ' letters long.') print('How old are you?') yourAge=input() print('Awesome you will be ' + str(int(yourAge) + 1) + ' years old next year.')

---
+++ Whatsyourname.py
@@ -0,0 +1,13 @@
+#Lets do a name exchange and try to talk smart
+print('Welcome to the basic naming conversation!')
+print('My name is Etc!')
+print('What is your name?')
+yourName=input()
+print('It is nice to meet you ' + yourName + '.')
+print('It appears so that your name is ' + str(len(yourName)) + ' letters long.')
+print('How old are you?')
+yourAge=input()
+print('Awesome you will be ' + str(int(yourAge) + 1) + ' years old next year.')
+
+
+
